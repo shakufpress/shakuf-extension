7192e967731ab520ba44df242501f7f1a6e11b44
idan haviv <idan.haviv@gmail.com>
2021-04-29 10:27:58 +0300

    mock data

--- src/background/api.js
+++ src/background/api.js
@@ -1,13 +1,23 @@
-import {HEBREW_LOOKUP} from "../constants";
+import { HEBREW_LOOKUP } from '../constants';
 
 const url = 'https://shakuf.press/hak/settings.json';
-const ext_rules_url = 'https://shakuf.press/hak/ext/ext_rules.json';
+// const ext_rules_url = 'https://shakuf.press/hak/ext/ext_rules.json';
+const ext_rules_url = 'https://shakuf.co.il/hak/ext/ext_rules.json?v=38293';
 const stagingUrl = 'https://shakuf.press/hak_staging/settings.json';
-const fetchInterval = 1000 * 60 * 60 * 24;
+// const fetchInterval = 1000 * 60 * 60 * 24;
+const fetchInterval = 1000 * 3;
 let data = {};
 let names = [];
-let rules = [];
-
+// let rules = [];
+let rules = [
+  {
+    id: '1',
+    domain: 'ynet.co.il',
+    selector: 'span[data-text=true]',
+    regexRule: 'בשיתוף',
+    ruleDescription: 'ההודעה הזו מופיעה כי יש תוסף וכו',
+  },
+];
 
 const decodeString = (str) => {
   return str.replace(/&#(\d+);/g, (match, number) => {
